17f5df1f615a3e3a1c22f23fd78bbe9578fc6171
Goeffrey Bell <evotekstl@gmail.com>
2017-06-19 17:21:53 -0500

    ChangeMaker

---
+++ src/isvalueGreaterthan75.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class isvalueGreaterthan75 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int q=0;int qc=0;
+		int d=0;int dc=0;
+		int n=0; int nc=0;
+		int p=0;int pc=0;
+		
+				
+		System.out.println("Enter your Change amount.");
+		Scanner sc = new Scanner(System.in);
+		int ui1 =sc.nextInt();
+		//for (ui1 > 25)
+		if(ui1 > 25)
+			//initialise; compare; increment
+			for(q=q+1; ;) {
+				ui1=(ui1 - 25);	
+				qc++;
+			}
+		}
+		if(ui1 > 10;d=d+1;dc ++){
+			ui1=ui1 - 10;
+			}
+		{
+				
+		}
+		if(ui1 > 5;n=n+1;nc ++) {
+			ui1=ui1 - 5;	
+		}
+		if(ui1 > 1;p=p+1;pc ++) {
+			ui1=ui1 - 1;	
+		}
+		System.out.println("you get, "+qc+" quarters, "+dc"dimes,\n");
+		System.out.println(n+"nickels, "+"and "+p" pennies.);
+	}
+
+		//private static void nextLine() {
+		// TODO Auto-generated method stub
+		
+	} 
+
+}
